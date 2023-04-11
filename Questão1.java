d4d45ff45c327c0b43bf07c2ba536981d30f0263
Matheus-Ryan <130509088+Matheus-Ryan@users.noreply.github.com>
2023-04-11 19:55:22 -0300

    Update Questão1.java

--- Questão1.java
+++ Questão1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Questao1{
     public static void main(String[] args) {
         Scanner receber = new Scanner(System.in);
         System.out.println("digite o primeiro numero: ");
